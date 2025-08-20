2a1708dd7abb54890a372b7df5ddc81b0cc6a512
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-08-20 11:21:11 +0530

    edit

--- src/routes/sales.router.js
+++ src/routes/sales.router.js
@@ -6,6 +6,6 @@ import { getSalesOverview } from "../controller/sales.controller.js";
 
 const salesRouter = express.Router();
 
-salesRouter.get("/overview", getSalesOverview);
+salesRouter.get("/", getSalesOverview);
 
 export default salesRouter;
